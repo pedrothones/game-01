27f9fba56a13d54ede2bb59f09cc86c11931892e
pedrothones <pedro.thones@gmail.com>
2022-09-07 21:41:03 -0300

    Coded the single round exercise to test the game

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function getComputerChoice() {
   randomNumber = Math.random() * 10;
-  console.log(randomNumber);
+
+  //mudar pra *10 para comecar jogo
   if (randomNumber <= 3.33) {
     randomOutput = "Rock";
   } else if (randomNumber >= 6.66) {
@@ -8,7 +9,48 @@ function getComputerChoice() {
   } else {
     randomOutput = "Paper";
   }
-  console.log(randomOutput);
 }
-
 getComputerChoice();
+let playerSelection = "Rock";
+let computerSelection = randomOutput;
+
+// playerSelection.toLowerCase() === computerSelection.toLowerCase();
+console.log("You choose " + playerSelection);
+console.log("Computer choose " + computerSelection);
+
+function singleRound() {
+  if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    console.log("Draw!");
+  } else if (
+    (playerSelection.toLowerCase() == "rock") &
+    (computerSelection.toLowerCase() == "scissors")
+  ) {
+    console.log("You win!");
+  } else if (
+    (playerSelection.toLowerCase() == "rock") &
+    (computerSelection.toLowerCase() == "paper")
+  ) {
+    console.log("You lose!");
+  } else if (
+    (playerSelection.toLowerCase() == "paper") &
+    (computerSelection.toLowerCase() == "rock")
+  ) {
+    console.log("You win!");
+  } else if (
+    (playerSelection.toLowerCase() == "paper") &
+    (computerSelection.toLowerCase() == "scissors")
+  ) {
+    console.log("You lose!");
+  } else if (
+    (playerSelection.toLowerCase() == "scissors") &
+    (computerSelection.toLowerCase() == "rock")
+  ) {
+    console.log("You lose!");
+  } else if (
+    (playerSelection.toLowerCase() == "scissors") &
+    (computerSelection.toLowerCase() == "paper")
+  ) {
+    console.log("You win!");
+  }
+}
+singleRound();
